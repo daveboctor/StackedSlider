f420c9ba0b23db2ad232bd00298242e80e60b322
Dave B <imageinteractive@outlook.com>
2024-07-29 12:24:11 -0400

    Replaced translateX with left property

--- js/gestures.js
+++ js/gestures.js
@@ -3,10 +3,6 @@ import * as MostVisible from "most-visible";
 
 const gestureEvents = "swipeleft swiperight panstart pan panend tap";
 
-// https://stackoverflow.com/questions/42267189/how-to-get-value-translatex-by-javascript
-// https://stackoverflow.com/questions/1183903/regex-using-javascript-to-return-just-numbers/42264780#42264780
-const NUMERIC_REGEXP = /[-]{0,1}[\d]*[,]?[\d]*[.]{0,1}[\d]+/g;
-
 let hammer;
 
 // Lists
@@ -15,7 +11,6 @@ let panelElList,
 
 // HTML element object
 let panelContainerEl,
-    panelInViewEl,
     targetPanelEl,
     prevPanelEl,
     nextPanelEl;
@@ -24,9 +19,9 @@ let panelContainerEl,
 let targetIndex,
     moveToIndex;
 
-let targetTranslateX,
-    prevTranslateX,
-    nextTranslateX;
+let targetStartX,
+    prevStartX,
+    nextStartX;
 
 let numOfPanels;
 
@@ -36,25 +31,22 @@ let panListenerIndex;
 let panning,
     swipeGestureExecuted;
 
-/* ------------------------------------------------------------
-    Gesture event listeners
- */
-
 // The plugin 'Hammer.js' (hammerjs.github.io) captures
 // the events of a web app's pointer (touch/mouse/pen).
 
 function on(container) {
     if (!container) { return; }
 
+    // If the 'hammer.min.js' plugin is not loaded, then exit
+    if (typeof Hammer === "undefined") { return; }
+
     panelContainerEl = container;
 
     panelElList = panelContainerEl.querySelectorAll(".panel");
+
     panelElArr = Array.from(panelElList);
 
     numOfPanels = panelElList.length;
-
-    // If the 'hammer.min.js' plugin is not loaded, then exit
-    if (typeof Hammer === "undefined") { return; }
 
     // Create a 'Hammer' instance.
     // By default the 'Hammerjs' library adds horizontal recognizers only.
@@ -70,20 +62,20 @@ function on(container) {
     hammer.on(gestureEvents, hammerListener);
 }
 
+function off() {
+    hammer.off(gestureEvents, hammerListener);
+}
+
 function hammerListener(e) {
     //console.log(e.type);
-    if (Math.abs(e.deltaY) > Math.abs(e.deltaX) && e.type !== "tap") { return; }
 
-    /*
-    if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-        if (targetPanelEl) {
-            targetPanelEl.style.zIndex = 100;
-            StackedSlider.moveTo(targetIndex);
-            reset();
-        }
-        return;
-    }
-    */
+    // If the gesture event is not a tap, and the size of the
+    // vertical pan delta is greater than the size of the
+    // horizontal pan delta, then the gesture is a vertical
+    // scroll. Therefore, exit this event listener.
+    // https://stackoverflow.com/questions/26677463/hammer-js-breaks-vertical-scroll-when-horizontal-pan/54587885#54587885
+    // https://stackoverflow.com/questions/53801499/hammer-js-how-to-block-horizontal-panning-during-page-scroll/53812660#53812660
+    if (e.type !== "tap" && Math.abs(e.deltaY) > Math.abs(e.deltaX)) { return; }
 
     switch (e.type) {
         case "tap":
@@ -96,7 +88,6 @@ function hammerListener(e) {
             StackedSlider.moveTo(moveToIndex);
             reset();
             break;
-
         case "swipeleft":
         case "swiperight":
             targetPanelEl = e.target.closest(".panel");
@@ -111,22 +102,27 @@ function hammerListener(e) {
             swipeGestureExecuted = true;
             reset();
             break;
-
         case "panstart":
-            panelInViewEl = panelContainerEl.querySelector(".panel-in-view");
-
-            const panelInViewIndex = panelElArr.indexOf(panelInViewEl);
-
             targetPanelEl = e.target.closest(".panel");
             if (!targetPanelEl) { return; }
 
             targetIndex = panelElArr.indexOf(targetPanelEl);
 
-            if (panelInViewIndex === targetIndex) {
+            const panelInViewEl = panelContainerEl.querySelector(".panel-in-view");
+            const panelInViewIndex = panelElArr.indexOf(panelInViewEl);
+
+            // The most common case is the pan gesture starts on
+            // the panel in view. That is, the target panel of the
+            // pan start gesture is the same as the panel containing
+            // the class name 'panel-in-view'.
+            if (targetIndex === panelInViewIndex) {
+                // If the target panel is greater than the first panel,
+                // then assign the previous panel element.
                 if (targetIndex > 0) {
                     prevPanelEl = panelElList[targetIndex - 1];
                 }
-
+                // If the target panel is less than the last panel,
+                // then assign the next panel element.
                 if (targetIndex < numOfPanels - 1) {
                     nextPanelEl = panelElList[targetIndex + 1];
                 }
@@ -142,52 +138,61 @@ function hammerListener(e) {
 
             panning = true;
 
-            let numbers;
-
-            numbers = targetPanelEl.style.transform.match(NUMERIC_REGEXP);
-            targetTranslateX = !numbers ? 0 : parseFloat(numbers[0]);
+            targetStartX = parseInt(targetPanelEl.style.left);
 
             if (prevPanelEl) {
-                numbers = prevPanelEl.style.transform.match(NUMERIC_REGEXP);
-                prevTranslateX = !numbers ? 0 : parseFloat(numbers[0]);
+                prevStartX = parseInt(prevPanelEl.style.left);
             }
 
             if (nextPanelEl) {
-                numbers = nextPanelEl.style.transform.match(NUMERIC_REGEXP);
-                nextTranslateX = !numbers ? 0 : parseFloat(numbers[0]);
+                nextStartX = parseInt(nextPanelEl.style.left);
             }
 
             break;
-
         case "pan":
+            // The 'panListenerIndex' variable is used to skip
+            // the first 'pan' type event gestures. This handles the
+            // case where the user is starts a vertical scroll
+            // (i.e. a pan in the vertical direction), but the 'hammer.js'
+            // plugin first reports the gesture as a horizontal pan
+            // right after the 'panstart' event gesture type is dispatched.
             if (!panListenerIndex || panListenerIndex < 3) {
                 panListenerIndex++;
                 return;
             }
+
             if (!targetPanelEl) { return; }
             if (swipeGestureExecuted) { return; };
             if (panning === false) { return; }
-            // hammerjs.github.io/api/#event-object
+
             if (e.direction === 1) { return; } // DIRECTION_NONE === 1
+            // https://hammerjs.github.io/api/#event-object
+            // Alternatively, check the direction value is one of the
+            // following valid directions: left, right, or horizontal.
+            // DIRECTION_LEFT === 2, DIRECTION_RIGHT === 4, DIRECTION_HORIZONTAL === 6
+            // const dirSet = new Set([2, 4, 6]);
+            // if (!dirSet.has(e.direction)) { return; }
 
-            let posn;
+            const deltaX = e.deltaX;
 
-            posn = targetTranslateX + e.deltaX;
-            targetPanelEl.style.transform = `translateX(${posn}px)`;
+            targetPanelEl.style.left = `${(targetStartX + deltaX)}px`;
 
             if (prevPanelEl) {
-                posn = prevTranslateX + e.deltaX;
-                prevPanelEl.style.transform = `translateX(${posn}px)`;
+                prevPanelEl.style.left = `${(prevStartX + deltaX)}px`;
             }
 
             if (nextPanelEl) {
-                posn = nextTranslateX + e.deltaX;
-                nextPanelEl.style.transform = `translateX(${posn}px)`;
+                nextPanelEl.style.left = `${(nextStartX + deltaX)}px`;
             }
 
             break;
-
         case "panend":
+            // A swipe gesture event type is triggered between a pan
+            // start and pan end event. Hammer.js dispatches the following:
+            // 'panstart', 'swipe[left,right]', 'pan', and finally, 'panend'.
+            // If the swipe gesture event listener processed the
+            // gesture event already, then there's no need to process
+            // the 'pan' and 'panend' events.
             if (swipeGestureExecuted) {
                 swipeGestureExecuted = false;
                 return;
@@ -199,20 +204,33 @@ function hammerListener(e) {
             }
 
             if (prevPanelEl || nextPanelEl) {
-                // https://stackoverflow.com/questions/38360676/get-the-element-which-is-the-most-visible-on-the-screen/39576399#39576399
-                // https://github.com/andyexeter/most-visible
                 const elemArr = [targetPanelEl];
                 if (prevPanelEl) { elemArr.push(prevPanelEl); }
                 if (nextPanelEl) { elemArr.push(nextPanelEl); }
-                if (elemArr.length > 1) {
-                    targetPanelEl.style.zIndex = numOfPanels;
+                if (elemArr.length > 1 &&
+                    !targetPanelEl.classList.contains("panel-in-view")) {
+                    targetPanelEl.style.zIndex = numOfPanels * 3;
                 }
+                // https://stackoverflow.com/questions/38360676/get-the-element-which-is-the-most-visible-on-the-screen/39576399#39576399
+                // https://github.com/andyexeter/most-visible
                 const mostVisibleEl = MostVisible.mostVisible(elemArr);
                 moveToIndex = panelElArr.indexOf(mostVisibleEl);
                 StackedSlider.moveTo(moveToIndex);
             }
             else {
-                targetPanelEl.style.zIndex = numOfPanels;
+                // This condition is executed (i.e. no value is assigned
+                // to the 'prevPanelEl' and 'nextPanelEl' variables)
+                // when the pan gesture targets a stacked panel that is
+                // not the panel in view or the direct neighbors
+                // (i.e. the panel to the immediate left or immediate right)
+                // of the panel in view. For example, if the panel in view is
+                // the first panel in the panel element array, and the
+                // pan gesture starts on the last panel in the slider
+                // stack, then only the 'targetPanelEl' variable is
+                // assigned.
+                if (!targetPanelEl.classList.contains("panel-in-view")) {
+                    targetPanelEl.style.zIndex = numOfPanels * 3;
+                }
                 StackedSlider.moveTo(targetIndex);
             }
 
@@ -248,24 +266,4 @@ function reset() {
     moveToIndex = undefined;
 }
 
-function off() {
-    hammer.off(gestureEvents, hammerListener);
-}
-
 export { on, off }
-
-
-// https://hammerjs.github.io/api/#event-object
-// DIRECTION_LEFT === 2, DIRECTION_RIGHT === 4, DIRECTION_HORIZONTAL === 6
-
-//const dirSet = new Set([2, 4, 6]);
-
-//if (!dirSet.has(e.direction)) { return; }
-
-// https://stackoverflow.com/questions/53801499/hammer-js-how-to-block-horizontal-panning-during-page-scroll/53812660#53812660
-//if (e.pointerType === 'touch' && (Math.abs(e.deltaY) > Math.abs(e.deltaX))) { return false; }
-
-// https://stackoverflow.com/questions/26677463/hammer-js-breaks-vertical-scroll-when-horizontal-pan/54587885#54587885
-//if (Math.abs(e.deltaY) > Math.abs(e.deltaX)) {
-//    return;
-//}
--- js/stacked-slider.js
+++ js/stacked-slider.js
@@ -8,12 +8,26 @@ let panelElList,
 
 let numOfPanels;
 
+let isInitializing;
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Resize_Observer_API
 const resizeObserver = new ResizeObserver(entries => {
+
     // 'entry' variable represents a 'ResizeObserverEntry' object.
     // https://developer.mozilla.org/en-US/docs/Web/API/ResizeObserverEntry
     for (const entry of entries) {
         if (!entry.target.classList.contains("panel-container")) { continue; }
+
+        // The 'resizeObserver' object is executed after the line
+        // 'resizeObserver.observe(panelContainerEl);' is processed
+        // in the 'on()' function. During this execution, the
+        // 'setPanelWidth()' function triggers another 'resizeObserver'
+        // object. To avoid duplication execution of the code
+        // within the callback function of the resizer observer constructor,
+        // use the 'isInitializing' boolean flag to exit the second/duplicate
+        // execution.
+        if (isInitializing === undefined) { isInitializing = true; }
+        else if (isInitializing) { isInitializing = false; return; }
 
         // Update width of each '.panel' element in 'panelElList'
         setPanelWidth(entry.target);
@@ -21,20 +35,17 @@ const resizeObserver = new ResizeObserver(entries => {
         // Update '.panel' spacing and position
         const panelInViewEl = panelContainerEl.querySelector(".panel-in-view");
         const moveToIndex = panelElArr.indexOf(panelInViewEl);
-        setPanelPosn(moveToIndex);
+        setPanelPosn(moveToIndex, false);
     }
 });
 
-// Width unit in 'px'
+// Width unit: 'px'
 const setPanelWidth = (panelContainerEl) => {
-    const panelContainerWidth =
-        parseInt(panelContainerEl.getBoundingClientRect().width, 10);
-    panelWidth = panelContainerWidth - ((numOfPanels - 1) * stackWidth);
+    panelWidth = panelContainerEl.getBoundingClientRect().width - ((numOfPanels - 1) * stackWidth);
     panelElList.forEach(panelEl => panelEl.style.width = `${panelWidth}px`);
 };
 
 const transitionendHandler = (e) => {
-    e.target.removeEventListener('transitionend', transitionendHandler);
     e.target.classList.remove('is-moving');
 };
 
@@ -47,13 +58,16 @@ function on(container, config) {
 
     // 'querySelectorAll()' returns a 'NodeList' object.
     // NodeList objects are the collection of nodes.
+    panelElList = panelContainerEl.querySelectorAll(".panel");
+
     // NodeList is not a JavaScript 'Array' although it's possible
     // to iterate over a 'NodeList' using the 'forEach()' method.
     // NodeList can be converted into a JavaScript array using 'Array.from()'.
-    panelElList = panelContainerEl.querySelectorAll(".panel");
     panelElArr = Array.from(panelElList);
 
     numOfPanels = panelElList.length;
+
+    panelElList.forEach(panelEl => panelEl.addEventListener("transitionend", transitionendHandler));
 
     // Set initial panel to display
     let moveToIndex = isNaN(config.moveToIndex) ? 0 : config.moveToIndex;
@@ -64,6 +78,9 @@ function on(container, config) {
     if (moveToPanelEl) { moveToPanelEl.classList.add("panel-in-view"); }
 
     // The observer "callback is firing immediately once observe() is called."
+    // See implementation notes within the resizer observer callback function
+    // and the use of the 'isInitializing' flag to avoid duplicate execution
+    // of the callback function on initialization.
     // https://stackoverflow.com/questions/60026223/does-resizeobserver-invoke-initially-on-page-load
     // https://stackoverflow.com/questions/67751039/javascript-resizeobserver-is-triggered-unexpected
     // https://github.com/WICG/resize-observer/issues/38#issuecomment-290537900
@@ -74,7 +91,8 @@ function off() {
     panelElList.forEach(panelEl => {
         panelEl.style.removeProperty("width");
         panelEl.style.removeProperty("z-index");
-        panelEl.style.removeProperty("transform");
+        panelEl.style.removeProperty("left");
+        panelEl.removeEventListener("transitionend", transitionendHandler);
     });
     resizeObserver.unobserve(panelContainerEl);
 }
@@ -82,61 +100,70 @@ function off() {
 function moveTo(moveToIndex, animate) {
     const panelInViewEl = panelContainerEl.querySelector(".panel-in-view");
 
-    const moveToPanelEl = panelElList[moveToIndex];
-
-    if (animate === false) {
-        if (panelInViewEl) { panelInViewEl.classList.remove("panel-in-view"); }
-        moveToPanelEl.classList.add("panel-in-view");
-        setPanelPosn(moveToIndex);
-        return;
-    }
-
     if (panelInViewEl) {
-        panelInViewEl.addEventListener('transitionend', transitionendHandler);
-        panelInViewEl.classList.add('is-moving'); // Trigger transform transition
         panelInViewEl.classList.remove('panel-in-view');
     }
 
+    const moveToPanelEl = panelElList[moveToIndex];
+
     if (moveToPanelEl) {
-        moveToPanelEl.addEventListener("transitionend", transitionendHandler);
-        moveToPanelEl.classList.add('is-moving', 'panel-in-view'); // Trigger transform transition
-        setPanelPosn(moveToIndex);
+        moveToPanelEl.classList.add('panel-in-view');
     }
+
+    setPanelPosn(moveToIndex, animate);
 }
 
-function setPanelPosn(moveToIndex) {
+// 'setPanelPosn()' sets the 'left' position of each '.panel'
+// element within the panel container parent element. The function
+// sets the 'z-index' position of each '.panel' element so that
+// each sliver (i.e. the thin left or right width of each '.panel'
+// element) overlaps the adjacent element.
+// The panel at the target index is set to the lowest 'z-index' value.
+// The panel sliver to the left should be higher than the
+// sliver to the right until the target index is reached.
+// The panel sliver higher than the target index should be lower
+// than the sliver to its right.
+function setPanelPosn(moveToIndex, animate) {
+    animate = animate !== false ? true : false;
+
     panelElList.forEach((panelEl, index) => {
         let posn;
-        let zIndex = numOfPanels;
-
-        // The panel at the target index is set to the lowest 'z-index' value.
-        // The panel sliver to the left should be higher than the
-        // sliver to the right until the target index is reached.
-        // The panel sliver higher than the target index should be lower
-        // than the sliver to its right.
 
         if (index < moveToIndex) {
             posn = panelWidth * -1 + (stackWidth * (index + 1));
-            zIndex += moveToIndex - index;
+            panelEl.style.zIndex = numOfPanels + moveToIndex - index;
         }
         else if (index > moveToIndex) {
             posn = panelWidth + (stackWidth * (index - 1));
-            zIndex += moveToIndex + index;
+            panelEl.style.zIndex = numOfPanels + moveToIndex + index;
         }
         else if (index === moveToIndex) {
             posn = stackWidth * index;
+            setTimeout(() => panelEl.style.zIndex = numOfPanels, 400);
         }
 
-        panelEl.style.transform = `translateX(${posn}px)`;
+        posn = parseInt(posn, 10);
 
-        panelEl.style.zIndex = zIndex;
+        // Convert string value of 'panelEl.style.left', which includes
+        // its units (e.g. "1024px"), to an integer value using
+        // 'parseInt()'. If the existing left position of the '.panel'
+        // element is the same as the value of the 'posn' variable,
+        // remove the 'is-moving' class in case this class name was
+        // not removed by the 'transitionendHandler()' function.
+        // The 'is-moving' class sets 'transition: left 0.4s;'.
+        // If the user gesture is panning, then the immediate effect
+        // of setting the 'left' value of a '.panel' element causes
+        // a lag effect when the 'is-moving' class is listed in the
+        // class name of a '.panel'.
+
+        if (parseInt(panelEl.style.left, 10) === posn) {
+            panelEl.classList.remove('is-moving');
+        }
+        else {
+            panelEl.style.left = `${posn}px`;
+            if (animate) { panelEl.classList.add('is-moving'); } // Trigger transition
+        }
     });
 }
 
 export { on, off, moveTo };
-
-
-//setTimeout(() => {
-//    const moveToPanelEl = panelElList[moveToIndex];
-//    moveToPanelEl.style.zIndex = numOfPanels;
-//}, 200);
